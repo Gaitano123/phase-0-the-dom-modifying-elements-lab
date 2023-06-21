13aa2672d0e243c6ad4777282e9914055eb3e03f
Gaitano <gaitano.george@student.moringaschool.com>
2023-06-22 00:54:48 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.getElementById('main')
+
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion'; 
+
+
+const container = document.querySelector('body');
+container.appendChild(newHeader);
